39200f04554a00dfaf3ba31f3080491a16cd96f9
Vlad <bahlaiv170307@gmail.com>
2026-06-01 18:59:26 +0300

    admin for folder habits

--- apps/habits/admin.py
+++ apps/habits/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Habit
 
-# Register your models here.
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'frequency', 'is_active', 'created_at')
+    list_filter = ('frequency', 'is_active')
+    search_fields = ('title', 'user__email')
+    ordering = ('-created_at',)
