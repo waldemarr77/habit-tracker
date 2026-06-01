f0949c32d8a3bb69388e5f320ad0432e4b4acbf4
Vlad <bahlaiv170307@gmail.com>
2026-06-01 18:29:06 +0300

    fix the bag in urls

--- apps/habits/urls.py
+++ apps/habits/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import HabitViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register('', HabitViewSet, basename='habit')
 
 urlpatterns = [
