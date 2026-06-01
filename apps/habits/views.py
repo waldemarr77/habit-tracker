a071fff115339f5101a40a32b7efa2ea88ce0f15
Vlad <bahlaiv170307@gmail.com>
2026-06-01 18:22:04 +0300

    views and urls for folder habits

--- apps/habits/views.py
+++ apps/habits/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Habit
+from .serializers import HabitSerializers
 
-# Create your views here.
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user, is_active=True)
