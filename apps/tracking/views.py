4a6077d0d12fed9f49b2c17f1534d01f4c9ad047
Vlad <bahlaiv170307@gmail.com>
2026-06-02 19:10:19 +0300

    views for tracking

--- apps/tracking/views.py
+++ apps/tracking/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import HabitCheckIn
+from .serializers import HabitCheckInSerializers
 
-# Create your views here.
+class CheckInListCreateView(generics.ListCreateAPIView):
+    serializer_class = HabitCheckInSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        habit_id = self.kwargs.get('habit_id')
+        return HabitCheckIn.objects.filter(
+            habit_id=habit_id,
+            habit__user=self.request.user
+        )
