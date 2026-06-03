a299a942d6152925d42af8a60e6f4fb78aae01a3
Vlad <bahlaiv170307@gmail.com>
2026-06-03 16:00:31 +0300

    the main urls

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+    path('api/auth/', include('apps.users.urls')),
+    path('api/habits/', include('apps.habits.urls')),
+    path('api/', include('apps.tracking.urls')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+
+    # фронтенд
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
